5233183b5c5e60e01d7b4da617bed69bbeba9c5f
CGstyling <christinaguseva@gmail.com>
2021-11-04 15:35:48 +0100

    profile page basics

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./Profile.css"
 
+
 function Profile() {
     return(
-        <>
-            <h1>Profiel pagina</h1>
-        </>
+        <div className="profile-container">
+
+            <div className="header-profile">
+                <h1>Profiel pagina</h1>
+                {/*<img src="" alt=""/>*/}
+                <h2>Gegevens:</h2>
+                <p>Gebruikersnaam:</p>
+                <p>Bio:</p>
+            </div>
+            <div className="buttons-profile">
+                <button>
+                    All recipes
+                </button>
+                <button>
+                    category
+                </button>
+                <button>
+                    edit profile
+                </button>
+                <hr/>
+            </div>
+
+            <div className="grid">
+
+            </div>
+        </div>
     );
 }
 
